f16ea10aadf8adba1d95155b765b0ec1b530b6e1
minilabo <ma_lu666@hotmail.com>
2018-12-13 00:09:29 +0900

    Add BroadFileSystemAccess to manifest. Add function to open File and Folder.

--- BroadFileSystemAccessTestApp/MainPage.xaml.cs
+++ BroadFileSystemAccessTestApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,9 +28,43 @@ namespace BroadFileSystemAccessTestApp
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
         {
+            var installFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
 
+            // set file path
+            // ex: var path = @"C:\targetfile"
+            var path = @"C:\Windows\DirectX.log";
+            this.textBlock.Text = String.Format("### Open {0} ###\n", path);
+            BroadFileOpen(path);
+        }
+
+        private async void BroadFileOpen(string path)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(path);
+            string text = await FileIO.ReadTextAsync(file);
+            this.textBlock.Text = text;
+        }
+
+        private void FolderOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var installFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+
+            // set folder path
+            // ex: var path = @"C:\"
+            var path = installFolder.Path;
+            this.textBlock.Text = String.Format("### Open {0} ###\n", path);
+            BroadFolderOpen(path);
+        }
+
+        private async void BroadFolderOpen(string path)
+        {
+            var folder = await StorageFolder.GetFolderFromPathAsync(path);
+            var files = await folder.GetFilesAsync();
+            foreach (var file in files)
+            {
+                this.textBlock.Text += file.Path + "\n";
+            }
         }
     }
 }
